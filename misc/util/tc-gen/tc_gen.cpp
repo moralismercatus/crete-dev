b0a80aef3656f35736b17f879f58827ba7cb5965
Christopher J. Havlicek <c.j.havlicek@gmail.com>
2017-03-07 02:48:59 -0800

    Improve tc-gen functionality
    - Add cmd line arguments.
    - Add ability to specify which config file to generate TC from.
    - Add ability to print ktest_pool TC files

--- misc/util/tc-gen/tc_gen.cpp
+++ misc/util/tc-gen/tc_gen.cpp
@@ -1,12 +1,23 @@
+/******************************************************************************
+ * Author(s): Christopher J. Havlicek
+ *
+ * For contact information, see CONTACTS.TXT
+ * For license information, see LICENSE.TXT
+ ******************************************************************************/
+
 #include "tc_gen.h"
 #include <crete/exception.h>
 
 #include <boost/property_tree/xml_parser.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/filesystem.hpp>
 
 #include <sstream>
 #include <fstream>
 #include <regex>
 
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 namespace pt = boost::property_tree;
 
 namespace crete
@@ -72,7 +83,6 @@ auto generate_test_case(const pt::ptree& complete_tree) -> TestCase
                 auto const& fill = *fill_opt;
                 auto sbyte = fill.get<std::string>("<xmlattr>.value");
                 auto isize = std::stoi(fill.get<std::string>("<xmlattr>.size"), 0, 16);
-                auto ibyte = 0u;
                 auto match = std::smatch{};
 
                 if(std::regex_search(sbyte,
@@ -105,6 +115,130 @@ auto generate_test_case(const pt::ptree& complete_tree) -> TestCase
     return rtc;
 }
 
+TestCaseUtility::TestCaseUtility(int argc, char *argv[])
+    : ops_descr_(make_options())
+{
+    parse_options(argc, argv);
+    process_options();
+}
+
+auto TestCaseUtility::parse_options(int argc, char* argv[]) -> void
+{
+    po::store(po::parse_command_line(argc, argv, ops_descr_), var_map_);
+    po::notify(var_map_);
+}
+
+auto TestCaseUtility::make_options() -> boost::program_options::options_description
+{
+    po::options_description desc("Options");
+
+    desc.add_options()
+        ("help,h", "displays help message")
+        ("config,c", po::value<fs::path>(), "XML describing test-case to generate")
+        ("out-file,o", po::value<fs::path>(), "output file (default: ./input_arguments.bin)")
+        ("test-case,t", po::value<fs::path>(), "test-case or test-case directory")
+            ;
+
+    return desc;
+}
+
+auto TestCaseUtility::process_options() -> void
+{
+    auto out_file = fs::path{"input_arguments.bin"};
+
+    if(var_map_.size() == 0)
+    {
+        cout << "Missing arguments" << endl;
+        cout << "Use '--help' for more details" << endl;
+        exit(0);
+    }
+    if(var_map_.count("help"))
+    {
+        std::cout << ops_descr_ << "\n";
+        std::cout << "Example --config file:\n"
+                  << "<crete>\n"
+                  << "\t<element>\n"
+                  << "\t\t<name>name1</name>\n"
+                  << "\t\t<data>deadbeef0123</data\n"
+                  << "\t<\element>\n"
+                  << "\t<element>\n"
+                  << "\t\t<name>name2</name>\n"
+                  << "\t\t<data>\n"
+                  << "\t\t\t<fill value=\"ff\" size=\"8\"/>\n"
+                  << "\t\t</data>\n"
+                  << "\t</element>\n"
+                  << "\t<element>\n"
+                  << "\t\t<name>name3</name>\n"
+                  << "\t\t<data>\n"
+                  << "\t\t\t<file>my_data_file.bin</file>\n"
+                  << "\t\t</data>\n"
+                  << "\t</element>\n"
+                  << "</crete>\n"
+                  << std::endl;
+        exit(0);
+    }
+    if(var_map_.count("out-file"))
+    {
+        out_file = var_map_["out-file"].as<fs::path>();
+
+        CRETE_EXCEPTION_ASSERT(fs::exists(out_file), err::file_missing{out_file.string()});
+    }
+    if(var_map_.count("config"))
+    {
+        auto config = var_map_["config"].as<fs::path>();
+
+        CRETE_EXCEPTION_ASSERT(fs::exists(config), err::file_missing{config.string()});
+
+        pt::ptree tree;
+        {
+            fs::ifstream ifs(config,
+                             std::ios::in | std::ios::binary);
+
+            pt::read_xml(ifs,
+                         tree);
+        }
+
+        auto tc = crete::util::generate_test_case(tree);
+
+        {
+            fs::ofstream ofs(out_file,
+                             std::ios::out | std::ios::binary);
+
+            crete::write_serialized(ofs, tc);
+        }
+
+        std::cout << tc << std::endl;
+    }
+    if(var_map_.count("test-case"))
+    {
+        auto tcp = var_map_["test-case"].as<fs::path>();
+
+        CRETE_EXCEPTION_ASSERT(fs::exists(tcp), err::file_missing{tcp.string()});
+
+        auto printer = [](const fs::path& tc)
+        {
+            std::cout << tc << ":\n";
+
+            fs::ifstream ifs(tc,
+                             std::ios::in | std::ios::binary);
+
+            std::cout << read_serialized(ifs) << "\n";
+        };
+
+        if(fs::is_directory(tcp))
+        {
+            for(const auto p : fs::directory_iterator(tcp))
+            {
+                printer(p);
+            }
+        }
+        else
+        {
+            printer(tcp);
+        }
+    }
+}
+
 } // namespace util
 } // namespace crete
 
@@ -112,17 +246,25 @@ int main(int argc, char* argv[])
 {
     try
     {
-    pt::ptree config;
-    {
-        std::ifstream ifs("tc.xml");
+         auto tc_util = crete::util::TestCaseUtility{argc, argv};
 
-        pt::read_xml(ifs,
-                     config);
-    }
+//        pt::ptree config;
+//        {
+//            std::ifstream ifs(argv[1]);
 
-    auto tc = crete::util::generate_test_case(config);
+//            pt::read_xml(ifs,
+//                         config);
+//        }
 
-    std::cerr << tc << std::endl;
+//        auto tc = crete::util::generate_test_case(config);
+
+//        {
+//            std::ofstream ofs("input_arguments.bin");
+
+//            crete::write_serialized(ofs, tc);
+//        }
+
+//        std::cerr << tc << std::endl;
     }
     catch(std::exception& e)
     {
--- misc/util/tc-gen/tc_gen.h
+++ misc/util/tc-gen/tc_gen.h
@@ -1,8 +1,16 @@
-#ifndef CRETE_TC_GEN_H
-#define CRETE_TC_GEN_H
+/******************************************************************************
+ * Author(s): Christopher J. Havlicek
+ *
+ * For contact information, see CONTACTS.TXT
+ * For license information, see LICENSE.TXT
+ ******************************************************************************/
+
+#ifndef CRETE_UTIL_TC_GEN_H
+#define CRETE_UTIL_TC_GEN_H
 
 #include <istream>
 
+#include <boost/program_options.hpp>
 #include <boost/property_tree/ptree.hpp>
 
 #include <crete/test_case.h>
@@ -14,8 +22,22 @@ namespace util
 
 auto generate_test_case(const boost::property_tree::ptree& complete_tree) -> TestCase;
 
-}
-}
+class TestCaseUtility
+{
+public:
+    TestCaseUtility(int argc, char* argv[]);
 
-#endif // CRETE_TC_GEN_H
+    auto parse_options(int argc, char* argv[]) -> void;
+    auto make_options() -> boost::program_options::options_description;
+    auto process_options() -> void;
+
+private:
+    boost::program_options::options_description ops_descr_;
+    boost::program_options::variables_map var_map_;
+};
+
+} // namespace util
+} // namespace crete
+
+#endif // CRETE_UTIL_TC_GEN_H
 
